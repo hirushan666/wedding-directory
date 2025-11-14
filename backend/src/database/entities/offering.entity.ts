26d4435981325610163c6ab9f701d176eff0f2e2
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-14 18:18:40 +0530

    dep

--- backend/src/database/entities/offering.entity.ts
+++ backend/src/database/entities/offering.entity.ts
@@ -15,7 +15,7 @@ export class OfferingEntity {
     @Column({ type: 'varchar', length: 20 })
     category: string;
 
-    @Column({ type: 'boolean', default: true })
+    @Column({ type: 'boolean', default: false })
     visible: boolean;
 
     @Column({ type: 'varchar', length: 10, nullable: true })
