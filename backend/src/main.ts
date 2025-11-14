5e3e7fb73a351cd2b41bc56cfc1da66aebac390c
Hirushan Wijesiriwardena <hirushanww@gmail.com>
2025-11-14 17:45:15 +0530

    backend deployed

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,17 +5,17 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3000', 'https://sayido.lk', 'https://sayido-flame.vercel.app'], // Allow only your Next.js app to access the API
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies to be sent with requests
-    optionsSuccessStatus: 204, // Some legacy browsers choke on 204
-  };
+  // const corsOptions: CorsOptions = {
+  //   origin: ['http://localhost:3000', 'https://sayido.lk', 'https://wedding-directory-ten.vercel.app'], // Allow only your Next.js app to access the API
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true, // Allow cookies to be sent with requests
+  //   optionsSuccessStatus: 204, // Some legacy browsers choke on 204
+  // };
 
   app.enableCors({
-  origin: true,
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
 })
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,57 +1,66 @@
-import { Controller, Post, Req, UseGuards, Res, HttpStatus,UnauthorizedException  } from "@nestjs/common";
+import {
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  Res,
+  HttpStatus,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RequestWithVisitor } from './request-with-visitor.interface';
 import { RequestWithVendor } from './request-with-vendor.interface';
-import { AuthService } from "./auth.service";
-import { LocalAuthGuard } from "./guards/local-auth.guard";
+import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
-    constructor (private readonly authService: AuthService){}
+  constructor(private readonly authService: AuthService) {}
 
-    @UseGuards(LocalAuthGuard)
-    @Post('loginVisitor')
-    loginVisitor(@Req() req: RequestWithVisitor, @Res() res: Response): void {
-        const visitor = req.visitor;
-        if (!visitor) {
-            throw new UnauthorizedException('Invalid credentials for visitor');
-        }
-
-        const { access_token } = this.authService.loginVisitor(visitor);
-
-        // Set access_token as a regular cookie (not HttpOnly, so it is accessible from frontend)
-        res.cookie('access_token', access_token, {
-            // domain: process.env.COOKIE_DOMAIN === 'localhost' ? 'localhost' : '.ngrok-free.dev',
-            httpOnly: false,
-            secure: process.env.COOKIE_SECURE === 'true',
-            sameSite: process.env.COOKIE_SAMESITE === 'lax' ? 'lax' : 'none',
-            maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-        });
-
-        // Return the access_token in the response as well
-        res.status(HttpStatus.OK).json({ message: 'Login successful' ,
-            access_token,
-         });
+  @UseGuards(LocalAuthGuard)
+  @Post('loginVisitor')
+  loginVisitor(@Req() req: RequestWithVisitor, @Res() res: Response): void {
+    const visitor = req.visitor;
+    if (!visitor) {
+      throw new UnauthorizedException('Invalid credentials for visitor');
     }
 
-    @UseGuards(LocalAuthGuard)
-    @Post('loginVendor')
-    loginVendor(@Req() req: RequestWithVendor, @Res() res: Response): void {
-        const vendor = req.vendor;
-        if (!vendor) {
-            throw new UnauthorizedException('Invalid credentials for vendor');
-        }
-        const { access_token } = this.authService.loginVendor(vendor);
-        res.cookie('access_tokenVendor', access_token, {
-            // domain: process.env.COOKIE_DOMAIN === 'localhost' ? 'localhost' : '.ngrok-free.dev',
-            httpOnly: false,
-            secure: process.env.COOKIE_SECURE === 'true',
-            sameSite: process.env.COOKIE_SAMESITE === 'lax' ? 'lax' : 'none',
-            maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
-        });
+    const { access_token } = this.authService.loginVisitor(visitor);
 
-        res.status(HttpStatus.OK).json({ message: 'Login successful',
-            access_token,
-        });
+    // Set access_token as a regular cookie (not HttpOnly, so it is accessible from frontend)
+    res.cookie('access_token', access_token, {
+      // domain: process.env.COOKIE_DOMAIN === 'localhost' ? 'localhost' : '.ngrok-free.dev',
+      httpOnly: false,
+      secure: process.env.COOKIE_SECURE === 'true',
+      sameSite: process.env.COOKIE_SAMESITE === 'lax' ? 'lax' : 'none',
+      maxAge: 24 * 60 * 60 * 1000, // 1 day expiration
+    });
+
+    // Return the access_token in the response as well
+    res
+      .status(HttpStatus.OK)
+      .json({ message: 'Login successful', access_token });
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('loginVendor')
+  loginVendor(@Req() req: RequestWithVendor, @Res() res: Response): void {
+    const vendor = req.vendor;
+    if (!vendor) {
+      throw new UnauthorizedException('Invalid credentials for vendor');
     }
+    const { access_token } = this.authService.loginVendor(vendor);
+    res.cookie('access_tokenVendor', access_token, {
+      // domain: process.env.COOKIE_DOMAIN === 'localhost' ? 'localhost' : '.ngrok-free.dev',
+      httpOnly: false,
+      secure: process.env.COOKIE_SECURE === 'true',
+      sameSite: 'none',
+      domain: process.env.COOKIE_DOMAIN,
+      maxAge: 86400000,
+    });
+
+    res
+      .status(HttpStatus.OK)
+      .json({ message: 'Login successful', access_token });
+  }
 }
